0438d32fddb79c635f9c1b938a2feb18c09d6269
consuti-tec <consuti@consuti.com.br>
2024-09-08 21:02:00 -0300

    first commit

---
+++ src/AppConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ColorTheme: "#000000",
+  UrlPortal: "https://escoladoesporte.com.br/redesporti/unidades/escola-de-esportes-clube-uba",
+  UrlPainel: "https://painel.escoladoesporte.com.br/",
+  TimeSplashScreen: 3000,
+};
